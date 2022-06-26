0280ccdc26ace181a1d0a34248a2216b1988c633
sankar.boro <sankar.boro@yahoo.com>
2022-06-26 14:15:39 +0530

    building

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -7,4 +7,4 @@
 
 # Set up the versions to be used
 # Don't export them as their used in the context of other calls
-coreth_version=${CORETH_VERSION:-'v0.1.0'}
+coreth_version=${CORETH_VERSION:-'v0.0.0-20220626083716-be3c1f08900b'}
